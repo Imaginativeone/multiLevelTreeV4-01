99538992d6986599de590185c0e2c993d72852ab
Doug Franklin <doug@dougfranklin.com>
2019-04-21 09:10:17 -0400

    Use of SelectionModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+/**
+ * Node for to-do item
+ */
+export class TodoItemNode {
+  children: TodoItemNode[];
+  item: string;
+}
+
+/**
+ * Flat to-do item node with expandable and level information
+ */
+export class TodoItemFlatNode {
+  item: string;
+  level: number;
+  expandable: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+  /** The selection for checklist */
+  checklistSelection = new SelectionModel<TodoItemFlatNode>(true /* multiple */);
+
   public appPages = [
     {
       title: 'Example 01',
